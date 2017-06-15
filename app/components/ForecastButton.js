b3dbf8f106b3524af9d79922004802d32e880553
Janos Murvai-Gaal <janos.murvaigaal@gmail.com>
2017-06-15 13:18:43 +0200

    Forecast Button pimped

--- app/components/ForecastButton.js
+++ app/components/ForecastButton.js
@@ -2,14 +2,23 @@ import React, { Component } from 'react';
 import {
     TouchableOpacity,
     StyleSheet,
+    Platform,
 
     Text,
     View
 } from 'react-native';
 import ForecastButtonStyle from './styles/ForecastButtonStyle';
-
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'; // https://github.com/oblador/react-native-vector-icons
 
 export default class ForecastButton extends Component {
+
+    fabIcon = (
+        <Icon name="weather-partlycloudy" size={24} color="#FFFFFF" />
+    );
+
+    forecastText = (
+        <Text style={ForecastButtonStyle.text}>Forecast</Text>
+    );
 
     constructor(props) {
         super(props)
@@ -22,8 +31,8 @@ export default class ForecastButton extends Component {
                 activeOpacity={0.4}
                 onPress={this.props.onTapped}>
 
-                <View>
-                    <Text style={ForecastButtonStyle.text}>Forecast</Text>
+                <View style={{flexDirection: 'row'}}>
+                    {Platform.OS==="android" ? this.fabIcon : this.forecastText}
                 </View>
             </TouchableOpacity>
         );
